c3576d87122a7c0332b374aa95a153a846c1d8ce
Stanley <wltrstnly@hotmail.com>
2021-11-02 11:21:10 -0600

    First commit

---
+++ src/main/java/com/kodigo/personalagenda/PersonalagendaApplication.java
@@ -0,0 +1,13 @@
+package com.kodigo.personalagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalagendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodigo/personalagenda/PersonalagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodigo.personalagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
